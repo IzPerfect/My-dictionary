ad19d883b0763b61b24967f720d02538f560714d
IzPerfect <yjwan8390@gmail.com>
2023-02-13 01:59:12 +0900

    pymysql stury

---
+++ pymysql/config.py
@@ -0,0 +1,10 @@
+'''
+    사용법: from config import *
+'''
+MYSQL_HOST = '127.0.0.1'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'maria'
+MYSQL_DATABASE = 'pymysql'
+MYSQL_CHARSET = 'utf8'
+MYSQL_AUTOCOMMIT = True
+MYSQL_TABLENAME = 'mytable'
